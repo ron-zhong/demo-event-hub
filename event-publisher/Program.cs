71cbbccd7b6217dbc8eab1ed9926dcca4ddebce8
Ron Zhong <ron.zhong@mims.com>
2023-11-07 22:19:59 +0800

    Update publisher and receiver

--- event-publisher/Program.cs
+++ event-publisher/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure.Identity;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using System.Text;
+
+Console.WriteLine("event-publisher: start sending events.....");
+
+// number of events to be sent to the event hub
+int numOfEvents = 100;
+
+// The Event Hubs client types are safe to cache and use as a singleton for the lifetime
+// of the application, which is best practice when events are being published or read regularly.
+// TODO: Replace the <EVENT_HUB_NAMESPACE> and <HUB_NAME> placeholder values
+EventHubProducerClient producerClient = new EventHubProducerClient(
+    "poc-event-namespace.servicebus.windows.net",
+    "poc-event-hub",
+    new DefaultAzureCredential());
+
+int i = 1;
+
+while (i <= numOfEvents)
+{
+    // Create a batch of events 
+    using EventDataBatch eventBatch = await producerClient.CreateBatchAsync();
+
+    while( i % 10 != 0)
+    {
+        Console.WriteLine($"New event {i} at {DateTime.Now.ToLongTimeString()}");
+
+        if (!eventBatch.TryAdd(
+            new EventData(
+                Encoding.UTF8.GetBytes($"Event {i} at {DateTime.Now.ToLongTimeString()}")
+                )
+            ))
+        {
+            // if it is too large for the batch
+            throw new Exception($"Event {i} is too large for the batch and cannot be sent.");
+        }
+
+        ++i;
+    }
+
+    try
+    {
+        // Use the producer client to send the batch of events to the event hub
+        await producerClient.SendAsync(eventBatch);
+        Console.WriteLine($"A batch of {i} events has been published.");
+    }
+    finally
+    {
+        //await producerClient.DisposeAsync();
+    }
+
+    Console.WriteLine("Press enter to restart");
+    Console.ReadLine();
+}
--- event-receiver/Program.cs
+++ event-receiver/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure.Identity;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Processor;
+using Azure.Storage.Blobs;
+using System.Text;
+
+Console.WriteLine("event-receiver: wait for events.....");
+
+// Create a blob container client that the event processor will use
+// TODO: Replace <STORAGE_ACCOUNT_NAME> and <BLOB_CONTATINAER_NAME> with actual names
+BlobContainerClient storageClient = new BlobContainerClient(
+    new Uri("https://poceventstorage.blob.core.windows.net/poc-event-storage"),
+    new DefaultAzureCredential());
+
+// Create an event processor client to process events in the event hub
+// TODO: Replace the <EVENT_HUBS_NAMESPACE> and <HUB_NAME> placeholder values
+var processor = new EventProcessorClient(
+    storageClient,
+    EventHubConsumerClient.DefaultConsumerGroupName,
+    "poc-event-namespace.servicebus.windows.net",
+    "poc-event-hub",
+    new DefaultAzureCredential());
+
+// Register handlers for processing events and handling errors
+processor.ProcessEventAsync += ProcessEventHandler;
+processor.ProcessErrorAsync += ProcessErrorHandler;
+
+while (true)
+{
+    // Start the processing
+    await processor.StartProcessingAsync();
+
+    // Wait for 30 seconds for the events to be processed
+    await Task.Delay(TimeSpan.FromSeconds(3));
+
+    // Stop the processing
+    await processor.StopProcessingAsync();
+}
+
+Task ProcessEventHandler(ProcessEventArgs eventArgs)
+{
+    // Write the body of the event to the console window
+    Console.WriteLine("\tReceived event: {0} at {1}",
+        Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray()),
+        DateTime.Now.ToLongTimeString());
+    //Console.ReadLine();
+    return Task.CompletedTask;
+}
+
+Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
+{
+    // Write details about the error to the console window
+    Console.WriteLine($"\tPartition '{eventArgs.PartitionId}': an unhandled exception was encountered. This was not expected to happen.");
+    Console.WriteLine(eventArgs.Exception.Message);
+    //Console.ReadLine();
+    return Task.CompletedTask;
+}
